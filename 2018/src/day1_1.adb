52329898855635bb409b1aba5279a10c0d70570c
Erwin Bronkhorst <erwinbronkhorst@gmail.com>
2018-12-02 23:49:57 +0100

    Day 1 (First part)

--- 2018/src/day1_1.adb
+++ 2018/src/day1_1.adb
@@ -1,6 +1,27 @@
-procedure Day1_1 is
+with Ada.Text_IO;              use Ada.Text_IO;
+with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
+with Ada.Strings.Unbounded;    use Ada.Strings.Unbounded;
 
+procedure Day1_1 is
+   Input  : File_Type;
+   Answer : Integer := 0;
 begin
-   --  Insert code here.
-   null;
+   Open (File => Input, Mode => In_File, Name => "data/day1.input");
+   loop
+      declare
+         Frequency : Integer;
+         Line : Unbounded_String := To_Unbounded_String (Get_Line (Input));
+      begin
+         Frequency := Integer'Value (To_String (Line));
+         Answer    := Answer + Frequency;
+      end;
+   end loop;
+
+exception
+   when End_Error =>
+      if Is_Open (Input) then
+         Close (Input);
+      end if;
+
+      Put_Line (Integer'Image (Answer));
 end Day1_1;
