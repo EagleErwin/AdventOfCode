7103e96ea6be51bc35974de1c2d40daa6c12481e
Erwin Bronkhorst <erwinbronkhorst@gmail.com>
2018-12-23 16:04:54 +0100

    Day 20 (First part)

--- 2018/src/day20_1.adb
+++ 2018/src/day20_1.adb
@@ -29,126 +29,124 @@ procedure Day20_1 is
       Close (File => Input);
    end Load_File;
 
+   procedure Print_Maze(Input : in Maze) is
+
+   begin
+      for R in Input'Range loop
+         for C in Input(R)'Range loop
+            if R = 0 and C = 0 then
+               Put("|" & "(0)");
+            else
+               if Input(R)(C) /= null then
+                  Put("|" & Integer'Image(Input(R)(C).Distance));
+               else
+                  Put("|" & "  ");
+               end if;
+            end if;
+         end loop;
+         Put_Line("|");
+         Put_Line("--------------------------------------------------------------");
+      end loop;
+      Put_Line("==================================================================");
+   end Print_Maze;
+
    procedure Parse_Lineair_String (Input     : in out Regex_String;
                                    Labyrinth : in out Maze;
+                                   Next_Root : in Coordinate_Access;
                                    Current_Pos : in out Coordinate_Access) is
       Current_Char : Character;
       Current_Distance : Integer;
+      New_Pos : Coordinate_Access;
    begin
       Current_Char := Element(Input, 1);
-      Current_Distance := Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance;
 
       while Current_Char = 'N' or Current_Char= 'E'
         or Current_Char = 'S' or Current_Char = 'W' loop
-         Current_Distance := Current_Distance + 1;
-         if Current_Char = 'N' then
-            Current_Pos.Y := Current_Pos.Y - 1;
-            if Labyrinth(Current_Pos.Y)(Current_Pos.X) = null then
-               Labyrinth(Current_Pos.Y)(Current_Pos.X) := new Coordinate'(X        => Current_Pos.X,
-                                                                          Y        => Current_Pos.Y,
-                                                                          Distance => -1,
-                                                                          Root     => Current_Pos.Root);
-            end if;
 
-            if Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance = -1 or else
-              Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance > Current_Distance then
-               Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance := Current_Distance;
-            end if;
+         Current_Distance := Current_Pos.Distance + 1;
+         New_Pos := new Coordinate'(X        => Current_Pos.X,
+                                    Y        => Current_Pos.Y ,
+                                    Distance => Current_Distance,
+                                    Root     => Next_Root);
+         if Current_Char = 'N' then
+            New_Pos.Y := New_Pos.Y - 1;
          elsif Current_Char = 'S' then
-            Current_Pos.Y := Current_Pos.Y + 1;
-            if Labyrinth(Current_Pos.Y)(Current_Pos.X) = null then
-               Labyrinth(Current_Pos.Y)(Current_Pos.X) := new Coordinate'(X        => Current_Pos.X,
-                                                                          Y        => Current_Pos.Y,
-                                                                          Distance => -1,
-                                                                          Root     => Current_Pos.Root);
-            end if;
-            if Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance = -1 or else
-              Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance > Current_Distance then
-               Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance := Current_Distance;
-            end if;
+            New_Pos.Y := New_Pos.Y + 1;
          elsif Current_Char = 'E' then
-            Current_Pos.X := Current_Pos.X + 1;
-            if Labyrinth(Current_Pos.Y)(Current_Pos.X) = null then
-               Labyrinth(Current_Pos.Y)(Current_Pos.X) := new Coordinate'(X        => Current_Pos.X,
-                                                                          Y        => Current_Pos.Y,
-                                                                          Distance => -1,
-                                                                          Root     => Current_Pos.Root);
-            end if;
-            if Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance = -1 or else
-              Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance > Current_Distance then
-               Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance := Current_Distance;
-            end if;
+            New_Pos.X := New_Pos.X + 1;
          elsif Current_Char = 'W' then
-            Current_Pos.X := Current_Pos.X - 1;
-            if Labyrinth(Current_Pos.Y)(Current_Pos.X) = null then
-               Labyrinth(Current_Pos.Y)(Current_Pos.X) := new Coordinate'(X        => Current_Pos.X,
-                                                                          Y        => Current_Pos.Y,
-                                                                          Distance => -1,
-                                                                          Root     => Current_Pos.Root);
-            end if;
-            if Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance = -1 or else
-              Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance > Current_Distance then
-               Labyrinth(Current_Pos.Y)(Current_Pos.X).Distance := Current_Distance;
-            end if;
+            New_Pos.X := New_Pos.X - 1;
          end if;
+
+         -- If the labyrinth does not yet contain a valid coordinate, fill
+         -- in the new one.
+         if Labyrinth(New_Pos.Y)(New_Pos.X) = null then
+            Labyrinth(New_Pos.Y)(New_Pos.X) := New_Pos;
+         end if;
+
+         -- If the new distance is smaller than the old one, or the old one is -1
+         -- Update the coordinate with the new data
+         if Labyrinth(New_Pos.Y)(New_Pos.X).Distance = -1 or else
+           Labyrinth(New_Pos.Y)(New_Pos.X).Distance > Current_Distance then
+            Labyrinth(New_Pos.Y)(New_Pos.X).Distance := Current_Distance;
+
+            Labyrinth(New_Pos.Y)(New_Pos.X).Root := New_Pos.Root;
+         end if;
+
          Delete(Input, 1, 1);
          Current_Char := Element(Input, 1);
+
+         Current_Pos := Labyrinth(New_Pos.Y)(New_Pos.X);
       end loop;
    end Parse_Lineair_String;
 
-   procedure Parse_String (Input     : in out Regex_String;
-                           Labyrinth : in out Maze;
-                           Root      : in Coordinate_Access;
-                           Current_Pos : in Coordinate_Access) is
+   procedure Parse_String (Input       : in out Regex_String;
+                           Labyrinth   : in out Maze;
+                           Next_Root   : in Coordinate_Access;
+                           Current_Pos : in out Coordinate_Access) is
       First_Char : Character;
       Working_String : Regex_String;
-      Intermediate_Pos : Coordinate_Access;
-      New_Root : Coordinate_Access;
    begin
       First_Char := Element(Input, 1);
       Working_String := Delete(Input, 1, 1);
 
       if First_Char = '^' then
          --Put_Line("Starting");
-         Parse_String(Input     => Working_String,
-                      Labyrinth => Labyrinth,
-                      Root    => Root,
+         Parse_String(Input       => Working_String,
+                      Labyrinth   => Labyrinth,
+                      Next_Root   => Next_Root,
                       Current_Pos => Current_Pos);
       elsif First_Char = '$' then
          --Put_Line("Finished");
          return;
       elsif First_Char = 'N' or First_Char = 'E'
         or First_Char = 'S' or First_Char = 'W' then
-         Intermediate_Pos := Current_Pos;
          Parse_Lineair_String(Input       => Input,
                               Labyrinth   => Labyrinth,
-                              Current_Pos => Intermediate_Pos);
+                              Next_Root   => Next_Root,
+                              Current_Pos => Current_Pos);
          Parse_String(Input       => Input,
                       Labyrinth   => Labyrinth,
-                      Root        => Root,
-                      Current_Pos => Intermediate_Pos);
+                      Next_Root   => Next_Root,
+                      Current_Pos => Current_Pos);
       elsif First_Char = '(' then
          --Put_Line("Starting branch");
-         New_Root := new Coordinate'(X        => Current_Pos.X,
-                                     Y        => Current_Pos.Y,
-                                     Distance => -1,
-                                     Root     => Root);
          Parse_String(Input       => Working_String,
                       Labyrinth   => Labyrinth,
-                      Root        => New_Root,
+                      Next_Root   => Current_Pos,
                       Current_Pos => Current_Pos);
       elsif First_Char = '|' then
          --Put_Line("OR");
          Parse_String(Input       => Working_String,
                       Labyrinth   => Labyrinth,
-                      Root        => Root,
-                      Current_Pos => Root);
+                      Next_Root   => Next_Root,
+                      Current_Pos => Labyrinth(Next_Root.Y)(Next_Root.X));
       elsif First_Char = ')' then
          --Put_Line("Finished branch");
          Parse_String(Input       => Working_String,
                       Labyrinth   => Labyrinth,
-                      Root        => Root.Root,
-                      Current_Pos => Root);
+                      Next_Root   => Next_Root.Root,
+                      Current_Pos => Current_Pos);
       else
          Put_Line("Unsupported character:" & Character'Image(First_Char));
       end if;
@@ -161,12 +159,11 @@ procedure Day20_1 is
    begin
       Current_Regex := Input;
 
-      Current_Pos.Root := Current_Pos;
       Result(Current_Pos.X)(Current_Pos.Y) := Current_Pos;
 
       Parse_String(Input       => Current_Regex,
                    Labyrinth   => Result,
-                   Root        => Current_Pos,
+                   Next_Root   => Current_Pos,
                    Current_Pos => Current_Pos);
       return Result;
    end Create_Maze;
@@ -184,26 +181,6 @@ procedure Day20_1 is
       return Result;
    end Find_Largest_Distance;
 
-   procedure Print_Maze(Input : in Maze) is
-
-   begin
-      for R in Input'Range loop
-         for C in Input(R)'Range loop
-            if R = 0 and C = 0 then
-               Put("|" & "(0)");
-            else
-               if Input(R)(C) /= null then
-                  Put("|" & Integer'Image(Input(R)(C).Distance));
-               else
-                  Put("|" & "   ");
-               end if;
-            end if;
-         end loop;
-         Put_Line("|");
-         Put_Line("--------------------------------------------------------------");
-      end loop;
-   end Print_Maze;
-
    Input_Regex : Regex_String;
    Input_Maze : Maze;
    Answer : Integer;
@@ -217,6 +194,4 @@ begin
    Answer := Find_Largest_Distance(Input => Input_Maze);
 
    Put_Line(Integer'Image(Answer));
-   -- 1062 too low
-   -- 3575 too high
 end Day20_1;
